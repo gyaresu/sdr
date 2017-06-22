00d3a5027be66254f64e5d3691d56e0a6829058e
Gareth <me@gareth.codes>
2017-06-22 11:45:31 +0200

    dump1090 install

--- first.sh
+++ first.sh
@@ -2,6 +2,7 @@ sudo apt-get update
 sudo apt-get install socat -y
 wget https://raw.githubusercontent.com/keenerd/rtl-sdr-misc/master/heatmap/heatmap.py
 git clone https://github.com/antirez/dump1090.git
-
+cd dump1090
+make
 
 
