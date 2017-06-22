76b965e4982eea47eb5f747ae705e6624968eeae
Gareth <me@gareth.codes>
2017-06-22 14:37:22 +0200

    adsb explicit folders

--- adsb.sh
+++ adsb.sh
@@ -1,10 +1,10 @@
 # ADSB presentation in Firefox
 if [ ! -d "dump1090" ]; then
     git clone https://github.com/antirez/dump1090.git
-    cd dump1090
 make
 fi
 
+cd ~/gnuradio-live-setup/dump1090
 ./dump1090  --net >/dev/null 2>&1 &
 firefox localhost:8080 >/dev/null 2>&1 &
 
