f54a8d2cc02d218df2aaf4afca707d1fcbd5bde6
Gareth <me@gareth.codes>
2017-06-22 11:48:34 +0200

    heatmap path

--- first.sh
+++ first.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install socat -y
-wget https://raw.githubusercontent.com/keenerd/rtl-sdr-misc/master/heatmap/heatmap.py -O ~/
+wget https://raw.githubusercontent.com/keenerd/rtl-sdr-misc/master/heatmap/heatmap.py -O ~/heatmap.py
 git clone https://github.com/antirez/dump1090.git
 cd dump1090
 make
