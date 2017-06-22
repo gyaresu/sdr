53027b0b6bdb5cf40180b3f54ffae82dcb7877d5
Gareth <me@gareth.codes>
2017-06-22 14:29:15 +0200

    do sanity checks for directories

--- adsb.sh
+++ adsb.sh
@@ -1,7 +1,10 @@
 # ADSB presentation in Firefox
-git clone https://github.com/antirez/dump1090.git
-cd dump1090
+if [ ! -d "dump1090"]; then
+    git clone https://github.com/antirez/dump1090.git
+    cd dump1090
 make
+fi
+
 ./dump1090  --net >/dev/null 2>&1 &
 firefox localhost:8080 >/dev/null 2>&1 &
 
