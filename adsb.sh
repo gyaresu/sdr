8311fd4ec03d5079d1b14971df139852613d7687
Gareth <me@gareth.codes>
2017-06-22 16:12:37 +0200

    path edits

--- adsb.sh
+++ adsb.sh
@@ -1,9 +1,10 @@
 # ADSB presentation in Firefox
+cd ~
 if [ ! -d "dump1090" ]; then
     git clone https://github.com/antirez/dump1090.git
 make
 fi
 
-cd ~/gnuradio-live-setup/dump1090
+cd ~/dump1090
 ./dump1090  --net >/dev/null 2>&1 &
 firefox localhost:8080 >/dev/null 2>&1 &
