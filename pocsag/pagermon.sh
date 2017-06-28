d09588fe4d9b8dff3eaa4fe643d01c0c109e249c
Gareth <me@gareth.codes>
2017-06-28 16:19:30 -0400

    pagermon edting

--- pocsag/pagermon.sh
+++ pocsag/pagermon.sh
@@ -6,3 +6,10 @@ sudo npm install pm2 -g
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 git clone https://github.com/davidmckenzie/pagermon
 cp ~/gnuradio-live-setup/pocsag/process.json ~/pagermon/server/process.json
+sudo chown -R $USER:$(id -gn $USER) /home/ubuntu/.config
+cd ~/pagermon/server
+npm install
+export NODE_ENV=production
+pm2 start process.json
+cd ~/pagermon/client
+npm install
