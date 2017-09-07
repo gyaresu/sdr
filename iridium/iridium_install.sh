8c8c7bedaff4dc85159c306b1397060654b52bc2
Gareth <me@gareth.codes>
2017-09-06 17:23:57 -0700

    iridium toolchain installation

--- iridium/iridium_install.sh
+++ iridium/iridium_install.sh
@@ -3,18 +3,25 @@
 # gareth@greenpeace.org
 # 2017-06-21
 
-sudo apt-get update
-#sudo apt-get upgrade --force-yes
-
 # Install the GNURadio Iridium Decoder
-git clone https://github.com/muccc/gr-iridium.git
-cd gr-iridium 
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-sudo ldconfig
+if [ $(dpkg-query -W -f='${Status}' iridium-extractor 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  cd $HOME
+  git clone https://github.com/muccc/gr-iridium.git
+  cd $HOME/gr-iridium 
+  mkdir build
+  cd build
+  cmake ..
+  make
+  sudo make install
+  sudo ldconfig
+fi
+
+if [ ! -d $HOME/iridium-toolkit ]; then
+  git clone https://github.com/muccc/iridium-toolkit.git
+  cd $HOME/iridium-toolkit
+  export PATH=$PATH:$HOME/iridium-toolkit
+fi
 
 # Setup hackrf_sweep
 
