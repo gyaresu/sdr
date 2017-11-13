546148e9fdf78aeb055591bdecaddefa8b8e5d30
Gareth <me@gareth.codes>
2017-11-13 09:38:42 -0800

    notes on git submodule installation

--- iridium/iridium_install.sh
+++ iridium/iridium_install.sh
@@ -3,6 +3,13 @@
 # gareth@greenpeace.org
 # 2017-06-21
 
+## If installing from source remember to 
+
+# $ git clone https://github.com/gyaresu/sdr
+# $ git submodule update --init --recursive
+# $ cd iridium 
+# $ sh iridium_install.sh
+
 # Install the GNURadio Iridium Decoder
 if [ $(dpkg-query -W -f='${Status}' iridium-extractor 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
   cd $HOME/sdr/iridium/gr-iridium 
